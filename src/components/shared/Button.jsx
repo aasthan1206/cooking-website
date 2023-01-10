65a15785bb277804770c3e55448e980bef1ec3da
aasthan1206 <aastha.nayak2002@gmail.com>
2023-01-10 22:41:31 +0530

    [an] - updated button

--- src/components/shared/Button.jsx
+++ src/components/shared/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Button = ({classname}) => {
   return (
     <div>
-      <button className= {`bg-primary text-white rounded-full px-4 py-2 font-heading font-black text-[12px] w-44 tracking-widest ${classname}`}>READ MORE</button>
+      <button className= {`bg-primary text-white rounded-full px-6 py-3 font-heading font-black text-[14px] w-48 tracking-widest ${classname}`}>READ MORE</button>
     </div>
   )
 }
