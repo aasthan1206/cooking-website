ee61d920b3e7222f99570b418899825d0a16f292
aasthan1206 <aastha.nayak2002@gmail.com>
2023-01-10 21:01:56 +0530

    [an] - Create Button

---
+++ src/components/shared/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    <div>
+      <button className='bg-primary text-white rounded-full px-4 py-2 font-heading font-extrabold text-[12px] w-36 tracking-widest'>READ MORE</button>
+    </div>
+  )
+}
+
+export default Button
