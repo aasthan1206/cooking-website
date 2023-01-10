9e8cd2083384bc605e55a4724713a9c01f2b7e79
aasthan1206 <aastha.nayak2002@gmail.com>
2023-01-10 22:46:57 +0530

    [an] adding hover effect

--- src/components/shared/Button.jsx
+++ src/components/shared/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Button = ({classname}) => {
   return (
     <div>
-      <button className= {`bg-primary text-white rounded-full px-6 py-3 font-heading font-black text-[14px] w-48 tracking-widest ${classname}`}>READ MORE</button>
+      <button className= {`bg-primary text-white rounded-full px-6 py-3 font-heading font-black text-[14px] w-48 tracking-widest hover:bg-white hover:text-primary ${classname}`}>READ MORE</button>
     </div>
   )
 }
